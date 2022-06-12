1466613fe43f39143ad03d32f83b7797f9faa1db
Aditi Acharya <aditi@gurzu.com>
2022-06-12 15:51:29 +0545

    catched custom exception on sentry

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 import { useState } from 'react';
 import './App.css';
+import * as Sentry from "@sentry/react";
 
 function App() {
   const [username, setUserName] = useState("")
   const [displayName, setDisplayName] = useState(false)
+  
+  const handleDisplayName = () =>{
+    try {
+      if(username === ""){
+      const msg = "You must enter a value to continue";
+      throw msg;
+      }
+      setDisplayName(true)
+    }
+    catch(e) {
+      Sentry.captureException(e);
+      console.error(e)
+    }
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         Sentry with React           
         <div className="container">      
           <input className="inputBox" type="text" name="name" placeholder='Enter your name' onChange={(e) =>setUserName(e.target.value)}/>
-          <button className="primaryButton" onClick={() =>setDisplayName(true)}>Enter</button>            
+          <button className="primaryButton" onClick={handleDisplayName}>Enter</button>            
         </div>
         {displayName && username &&
           <span className="message">Hi 👋 {username}, welcome to Beginning of this Project.</span>
